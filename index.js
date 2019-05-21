b7cacae673b72cedc44d6dae4122c82d38dff6bb
Anthony Eriksen <ajrayid@gmail.com>
2019-05-21 07:33:16 -0600

    Finished title SVG animation, working on scroll animations and behavior

--- index.js
+++ index.js
@@ -10,16 +10,37 @@ cartButton.addEventListener('click', e => {
 }});
 
 
-$(".letter").css({
-  position: "relative",
-}).animate({
-  right: 999,
-}).animate({
-  right: 0,
-  opacity: .5,
-}, 1800, "swing").animate({
-  opacity: .9,
-},1000)
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <=0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'letter'
+        );
+      } 
+    
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
 
 
 
