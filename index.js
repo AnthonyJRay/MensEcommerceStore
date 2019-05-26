87c2d2c091da8cdac0c7f22c0e8f0634a02f996a
Anthony Eriksen <ajrayid@gmail.com>
2019-05-26 13:29:54 -0600

    Experimenting with JavaScript

--- index.js
+++ index.js
@@ -9,7 +9,51 @@ cartButton.addEventListener('click', e => {
   shoppingCart.classList.add('open');
 }});
 
+const contentArea = document.querySelector('.content-area');
 
+function getBoundingClientRect() {
+  const rect = contentArea.getBoundingClientRect();
+  return {
+    top: rect.top,
+    right: rect.right,
+    bottom: rect.bottom,
+    left: rect.left,
+    width: rect.width,
+    height: rect.height,
+    x: rect.x,
+    y: rect.y
+  };
+}
+
+// const contentTop = getBoundingClientRect(contentArea).top;
+// const pageTop = document.body.scrollTop;
+// const boxDiff = 49.5;
+
+// document.onscroll = scrollView();
+
+// function scrollView () {
+//   const contentTop = getBoundingClientRect(contentArea).top;
+//   const pageTop = document.body.scrollTop;
+//   if (pageTop + contentTop - 599 === 49.5) {
+//       console.log('Box is centered');
+//   }
+// }
+// scrollView()
+// scrollView(contentTop, pageTop);
+const contentTop = getBoundingClientRect(contentArea).top;
+const pageTop = document.body.scrollTop;
+const letter = document.querySelectorAll('.content-wrapper');
+
+$(document.body).scroll( function () {
+  if (pageTop + contentTop - 599 === 49.5) {
+      console.log('Box is centered');
+      letter.classList.replace (
+        'content-wrapper',
+        'letter'
+      );
+      console.log(letter)
+    }
+})
 
 
 
